b51eb192d39f8cdefebe798d02deac605babd484
Kate Rebello <katerebello27@gmail.com>
2021-09-19 17:41:29 +0530

    added

---
+++ env.js
@@ -0,0 +1,14 @@
+// const PRIVATE_KEY='71b9bb01ef015b02b13fb1dd7d5e403cec640242de0e8f231b1caf8e9753d9ef' //this is a dummy private key
+
+const PRIVATE_KEY = 'Enter your private key here'
+
+const TOKEN_ADDRESS='0x2859e4544c4bb03966803b044a93563bd2d0dd4d'; // change to Token address you want to target
+const AMOUNT=0.01
+const LEVEL=0.1
+
+module.exports = {
+    PRIVATE_KEY,
+    TOKEN_ADDRESS,
+    AMOUNT,
+    LEVEL
+};
